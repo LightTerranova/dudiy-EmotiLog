aca671a423f027c985a60557fbb01b1989075b31
LightTerranova <dudiy@ualberta.ca>
2025-09-30 19:07:59 -0600

    changing dao

--- code/dudiy-EmotiLog/app/src/main/java/com/example/dudiy_emotilog/AppDatabase.java
+++ code/dudiy-EmotiLog/app/src/main/java/com/example/dudiy_emotilog/AppDatabase.java
@@ -2,9 +2,7 @@ package com.example.dudiy_emotilog;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-
-// Creating an object to store the DAOs
 @Database(entities = {Emoji.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract EmojiDAO emojiDAO();
+    public abstract EmojiDao emojiDao();
 }
