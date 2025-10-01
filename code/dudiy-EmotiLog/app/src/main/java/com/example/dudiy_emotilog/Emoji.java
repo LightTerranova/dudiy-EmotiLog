94d023a05844bc8d4acd547ec3851329ea2f9383
LightTerranova <dudiy@ualberta.ca>
2025-09-30 19:08:41 -0600

    m

--- code/dudiy-EmotiLog/app/src/main/java/com/example/dudiy_emotilog/Emoji.java
+++ code/dudiy-EmotiLog/app/src/main/java/com/example/dudiy_emotilog/Emoji.java
@@ -2,11 +2,11 @@ package com.example.dudiy_emotilog;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-// Emoji to put into a db with unique pk
 @Entity
 public class Emoji {
     @PrimaryKey(autoGenerate = true)
     public int id;
+
     public String mood;
     public long timestamp;
 }
